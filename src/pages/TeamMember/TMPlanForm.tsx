9429c73f56577d5927be219279c5f2490859b78f
DmitryMass <dimamikron41@gmail.com>
2023-04-19 17:49:46 +0300

    hidden form for TeamMember with admin role

--- src/pages/TeamMember/TMPlanForm.tsx
+++ src/pages/TeamMember/TMPlanForm.tsx
@@ -1,22 +1,28 @@
+import useTypedSelector from '@/store/storeHooks/useTypedSelector';
 import { FC } from 'react';
 
 const TMPlanForm: FC = () => {
+  const user = useTypedSelector((state) => state.persistSlice.authData);
   return (
-    <div className='bg-gray bg-opacity-20 w-full rounded-[6px] p-[20px] mb-[20px]'>
-      Test Form
-      <div className='flex flex-col gap-[5px] text-black'>
-        <select defaultValue={'ww'} name='' id=''>
-          <option value='def'>Default </option>
-          <option value='2'>2</option>
-          <option value='3'>3</option>
-        </select>
-        <input type='text' placeholder='random text' />
-        <input type='text' placeholder='random text' />
-        <input type='text' placeholder='random text' />
-        <input type='text' placeholder='random text' />
-        <input type='text' placeholder='random text' />
-      </div>
-    </div>
+    <>
+      {user?.role === 'admin' ? (
+        <div className='bg-gray bg-opacity-20 w-full rounded-[6px] p-[20px] mb-[20px]'>
+          Test Form
+          <div className='flex flex-col gap-[5px] text-black'>
+            <select defaultValue={'ww'} name='' id=''>
+              <option value='def'>Default </option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+            </select>
+            <input type='text' placeholder='random text' />
+            <input type='text' placeholder='random text' />
+            <input type='text' placeholder='random text' />
+            <input type='text' placeholder='random text' />
+            <input type='text' placeholder='random text' />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
